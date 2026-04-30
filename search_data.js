27d13994fe6ad991359a6b944342159e661b510a
chv48v89rw-ui <terrogachie@gmail.com>
2026-04-30 11:23:47 +0300

    fourth

---
+++ search_data.js
@@ -0,0 +1,20 @@
+const foodData = [
+    { name: "Breakfast", link: "Breakfast.html" },
+    { name: "Pancakes", link: "Breakfast.html" },
+    { name: "Omelette", link: "Breakfast.html" },
+
+    { name: "Soups", link: "Soups.html" },
+    { name: "Chicken Soup", link: "Soups.html" },
+
+    { name: "Pasta", link: "Pasta.html" },
+    { name: "Chicken Alfredo", link: "Pasta.html" },
+
+    { name: "Beef", link: "Beef.html" },
+    { name: "Beef Steak", link: "Beef.html" },
+
+    { name: "Chicken", link: "Chicken.html" },
+    { name: "Grilled Chicken", link: "Chicken.html" },
+
+    { name: "Fish", link: "Fish.html" },
+    { name: "Fillet", link: "Fish.html" }
+];
